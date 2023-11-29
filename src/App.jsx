b064aa048d973e2d30bb77d87c4fb736204104e3
Gonzalo <34680110ax@gmail.com>
2023-11-29 11:36:10 -0300

    winner condition

--- src/App.jsx
+++ src/App.jsx
@@ -21,12 +21,48 @@ const Square = ({children, isSlected, updateBoard, index})=>{
   )
 }
 
+
+  const winningCombination = [
+    [0,1,2], // row 1
+    [3,4,5], // row 2
+    [6,7,8], // row 3
+    [0,3,6], // col 1
+    [1,4,7], // col 2
+    [2,5,8], // col 3
+    [0,4,8], // diagonal 1
+    [2,4,6], // diagonal 2
+  ]
+
+ 
+
 function App() {
   const  [board, setBoard] = useState(Array(9).fill(null))
 
   const [turn, setTurn] = useState(Turns.X)
+  const [wineer, setWineer] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    for(const combo of winningCombination){
+      const [a,b,c] = combo
+      console.log(combo)
+      if(
+        boardToCheck[a] && 
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]){
+          console.log(boardToCheck[a])
+
+        return boardToCheck[a]
+      }
+      
+    }
+    return null
+  } 
+
 
   const updateBoard = (index) => {
+    if(board[index] || wineer){
+      return
+    }
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
@@ -34,6 +70,11 @@ function App() {
 
     const newTurn = turn === Turns.X ? Turns.O : Turns.X
     setTurn(newTurn)
+
+    const winner = checkWinner(newBoard)
+    if(winner){
+      setWineer(winner)
+    }
   }
 
   return (
